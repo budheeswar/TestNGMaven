f9b5ec7bddd3e81047736f8d787e71fead2faf18
buddeeswar <Buddeeswar.Rajolla.-ND@disney.com>
2022-07-26 16:59:44 +0530

    Assertions added to FirstTestClass

--- src/test/java/com/rb/first/FirstTestClass.java
+++ src/test/java/com/rb/first/FirstTestClass.java
@@ -4,7 +4,9 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
 import org.testng.annotations.Test;
+import org.testng.asserts.SoftAssert;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 
@@ -18,10 +20,12 @@ public class FirstTestClass {
 		driver.manage().window().maximize();
 		driver.get("https://www.google.com/");
         driver.findElement(By.name("q")).sendKeys("selenium java", Keys.ENTER);
-        System.out.println(driver.getTitle());
-        Thread.sleep(5000);
+        //System.out.println(driver.getTitle());
+        String expectedTitle="selenium java - Google Search";
+        String actualTitle=driver.getTitle();
+        Assert.assertEquals(actualTitle, expectedTitle); // if this fails driver.quit wont execute as we using hard assertions        
         driver.quit();
-        System.out.println("test Google Executed");
+        //System.out.println("test Google Executed");
 		
 	}
 	@Test
@@ -32,10 +36,19 @@ public class FirstTestClass {
 		driver.get("https://www.facebook.com/");
 		Thread.sleep(5000);
 		driver.findElement(By.name("email")).sendKeys("facebook@email", Keys.ENTER);
-        System.out.println(driver.getTitle());
-        Thread.sleep(5000);
+		//driver.findElement(By.name("pass")).sendKeys("facebook@email", Keys.ENTER);
+		
+        //System.out.println(driver.getTitle());
+        //Thread.sleep(5000);
+		SoftAssert softAssert=new SoftAssert();
+		String actualTitle=driver.getTitle();
+		String expectedTitle="Facebook - log in or sign up";
+		softAssert.assertEquals(expectedTitle, actualTitle);
+		softAssert.assertEquals("same","same");
+		//softAssert.assertEquals("different","same","string mismatch");
         driver.quit();
         System.out.println("testFacebook Executed");
+        softAssert.assertAll();
 	}
 	
 }
